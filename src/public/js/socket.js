ad2f428dbbb5771c5a3249d9dbdd4cd2e9a472de
dahan-schuster <dan.plschuster@gmail.com>
2022-06-11 19:58:07 -0300

    refat: transforms socket into a subject

--- src/public/js/socket.js
+++ src/public/js/socket.js
@@ -1,24 +1,48 @@
-function createSocketConnection(game) {
+function createSocketConnection() {
     const socket = io();
+
+    const observers = {
+        onSetup: [],
+    }
 
     socket.on('connect', () => {
         console.log('Connected with ID ' + socket.id)
     })
 
     socket.on('setup', (state) => {
-        game.setState(state)
+        notify('onSetup', state)
     })
 
     socket.on('add-player', (player) => {
-        game.addPlayer(player)
+        notify('onNewPlayer', player)
     })
 
-    socket.on('card-card', (player) => {
-        game.addPlayer(player)
+    socket.on('remove-player', (playerId) => {
+        notify('onRemovePlayer', playerId)
     })
 
+    function subscribe(event, observerFunction) {
+        if (typeof observerFunction === "function") {
+            if (observers[event] !== undefined) {
+                observers[event].push(observerFunction);
+            } else {
+                throw new Error("Socket event " + event + " not found")
+            }
+        }
+    }
+
+    function notify(event) {
+        if (observers[event] === undefined)
+            return;
+
+        for (const observerFunction of observers[event]) {
+            typeof observerFunction === "function" &&
+                observerFunction()
+        }
+    }
 
     return {
+        subscribe,
         socket
     }
 }
